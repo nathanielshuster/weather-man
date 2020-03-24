7f6974910c0337d585e893b402578dd3a1463841
Nate Shuster <nshu521992@gmail.com>
2020-03-24 15:33:41 -0400

    creating site structure

--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
-import { Home } from './Home'
-import { About } from './About'
-import { Results } from './Results'
-import { NoMatch } from './NoMatch'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { Home } from './Home';
+import { About } from './About';
+import { Results } from './Results';
+import { NoMatch } from './NoMatch';
+import { Layout } from './components/Layout';
+import { NavigationBar } from './components/NavigationBar'
 
 class App extends Component {
   render() {
     return (
       <React.Fragment>
-        <Router>
-          <Switch>
-            <Route exact path="/" component={Home} />
-            <Route exact path="/results" component={Results} />
-            <Route exact path="/about" component={About} />
-            <Route component={NoMatch} />
-          </Switch>
-        </Router>
+        <NavigationBar/>
+        <Layout>
+          <Router>
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route exact path="/results" component={Results} />
+              <Route exact path="/about" component={About} />
+              <Route component={NoMatch} />
+            </Switch>
+          </Router>
+        </Layout>
       </React.Fragment>
     )
   }
---
+++ src/components/Jumbotron.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Jumbotron as Jumbo, Container } from 'react-bootstrap';
+import styled from 'styled-components';
+
+const Styles = styled.div`
+
+`;
+
+export const Jumbotron = () => (
+  <Styles>
+    <Jumbo>
+      
+    </Jumbo>
+  </Styles>
+)
