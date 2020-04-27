26f58a8967a7f559d33246df7a024749938aeea3
Nate Shuster <nshu521992@gmail.com>
2020-04-27 15:08:43 -0400

    spinner

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import { NoMatch } from './NoMatch'
 import { Layout } from './components/Layout'
 import { NavigationBar } from './components/NavigationBar'
 import { Footer } from './components/Footer'
+import { Spinner } from 'react-bootstrap'
 
 const API_KEY = "bc56cc6b51e6d5970645d40b8f469a84"
 
@@ -15,18 +16,20 @@ class App extends Component {
   constructor(props) {
     super(props)
     this.state = {
+      isLoading: false,
       city: undefined,
       temperature: undefined,
       feelsLike: undefined,
       high: undefined,
       low: undefined,
-      image: undefined,
+      imageCode: undefined,
       error: undefined
     };
   }
 
   getWeather = (e) => {
     e.preventDefault()
+    this.setState({ isLoading: true });
     const zip = e.target.zipCode.value
     this.props.history.push(`/weather/${zip}`)
     const url = `http://api.openweathermap.org/data/2.5/weather?zip=${zip},us&appid=${API_KEY}&units=imperial`
@@ -38,7 +41,8 @@ class App extends Component {
         feelsLike: Math.round(data.main.feels_like),
         high: Math.round(data.main.temp_max),
         low: Math.round(data.main.temp_min),
-        image: data.weather[0].icon
+        imageCode: data.weather[0].icon,
+        isLoading: false
       }))
   }
 
@@ -53,16 +57,19 @@ class App extends Component {
                   getWeather={this.getWeather}
                 />}
               />
-              <Route exact path="/weather/:zipCode" render={() =>
-                <Weather
-                  city={this.state.city}
-                  temperature={this.state.temperature}
-                  feelsLike={this.state.feelsLike}
-                  high={this.state.high}
-                  low={this.state.low}
-                  image={this.state.image}
-                />}
-              />
+              {
+                !this.state.isLoading ?
+                <Route exact path="/weather/:zipCode" render={(props) =>
+                  <Weather
+                    city={this.state.city}
+                    temperature={this.state.temperature}
+                    feelsLike={this.state.feelsLike}
+                    high={this.state.high}
+                    low={this.state.low}
+                    imageCode={this.state.imageCode}
+                  />}
+                /> : null
+              }
               <Route exact path="/about" component={About} />
               <Route component={NoMatch} />
             </Switch>
---
+++ src/Loading.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Container, Row, Col, Spinner } from 'react-bootstrap'
+import styled from 'styled-components';
+
+const Styles = styled.div`
+
+`;
+
+export const Loading = (props) => (
+  <Styles>
+    <Container>
+      <Row>
+        <Col>
+
+        </Col>
+      </Row>
+    </Container>
+  </Styles>
+)
